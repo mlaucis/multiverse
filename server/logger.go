c6a3c2046ea590d066f7f71448ee66d8f5d8f3b4
Florin Patan <florinpatan@gmail.com>
2014-12-26 16:31:46 +0100

    Add headers to the logs

--- server/logger.go
+++ server/logger.go
@@ -18,12 +18,18 @@ func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		// we should use this to sanitize any other headers that should not be exposed to the logs
+		headers := r.Header
+
 		inner.ServeHTTP(w, r)
 
+		// TODO sanitize headers that shouldn't not appear in the logs
+
 		log.Printf(
-			"%s\t%s\t%s\t%s\n",
+			"%s\t%s\t%+v\t%s\t%s\n",
 			r.Method,
 			r.RequestURI,
+			headers,
 			name,
 			time.Since(start),
 		)
