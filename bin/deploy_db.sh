2f08df906c05c4fe7831f6cf19bd93158b760845
Florin Patan <florinpatan@gmail.com>
2015-01-04 03:08:16 +0100

    More test framework improvements

--- bin/deploy_db.sh
+++ bin/deploy_db.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mysql -u ${WERCKER_MYSQL_USERNAME} -p${WERCKER_MYSQL_PASSWORD} -h ${WERCKER_MYSQL_HOST} -P ${WERCKER_MYSQL_PORT} ${WERCKER_MYSQL_DATABASE} < ${GOPATH}/src/github.com/tapglue/backend/resources/sql/tapglue.sql
+export WERCKER_SOURCE_DIR=${GOPATH}/src/github.com/tapglue/backend
+sed -i "s/CURRENT_TIMESTAMP/'2015-01-01 01:23:45'/g" ${WERCKER_SOURCE_DIR}/resources/sql/tapglue.sql
+mysql -u ${WERCKER_MYSQL_USERNAME} -p${WERCKER_MYSQL_PASSWORD} -h ${WERCKER_MYSQL_HOST} -P ${WERCKER_MYSQL_PORT} ${WERCKER_MYSQL_DATABASE} < ${WERCKER_SOURCE_DIR}/resources/sql/tapglue.sql
