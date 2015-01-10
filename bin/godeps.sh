4da7beebb335f9407a8929dd64c8f5bc4d006a55
Florin Patan <florinpatan@gmail.com>
2015-01-10 16:57:33 +0100

    Speed up building

--- bin/godeps.sh
+++ bin/godeps.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-go get -v -u github.com/tools/godep
+if test -d "$WERCKER_CACHE_DIR/go-pkg-cache"
+then
+    rsync -avzv --exclude "$WERCKER_SOURCE_DIR" "$WERCKER_CACHE_DIR/go-pkg-cache/" "$GOPATH/"
+else
+    go get -v -u github.com/tools/godep
+fi
+rsync -avzv --exclude "$WERCKER_SOURCE_DIR" "$GOPATH/" "$WERCKER_CACHE_DIR/go-pkg-cache/"
 cp -R $GOPATH/src/github.com/Tapglue $GOPATH/_src
 godep restore
 mv $GOPATH/_src $GOPATH/src/github.com/tapglue
