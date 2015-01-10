8cf873db265b3bdf9bd39cb761ad0fa3d328e42f
Florin Patan <florinpatan@gmail.com>
2015-01-10 18:02:10 +0100

    remove wercker cache for now

--- bin/godeps.sh
+++ bin/godeps.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-if test -d "$WERCKER_CACHE_DIR/go-pkg-cache"
-then
-    rsync -avzv --exclude "$WERCKER_SOURCE_DIR" "$WERCKER_CACHE_DIR/go-pkg-cache/" "$GOPATH/"
-else
-    go get -v -u github.com/tools/godep
-fi
+go get -v -u github.com/tools/godep
 rsync -avzv --exclude "$WERCKER_SOURCE_DIR" "$GOPATH/" "$WERCKER_CACHE_DIR/go-pkg-cache/"
 cp -R $GOPATH/src/github.com/Tapglue $GOPATH/_src
 godep restore
