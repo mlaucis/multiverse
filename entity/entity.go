d3e31919ae522431439d846993de08a9303b8161
Onur Akpolat <onur.akpolat@gmail.com>
2015-01-15 16:38:50 +0100

    Updated entities

--- entity/entity.go
+++ entity/entity.go
@@ -9,131 +9,157 @@ package entity
 import "time"
 
 type (
+	// Commenly used structure
+	Common struct {
+		Image     []*Image
+		Metadata  string    `json:"metadata,omitempty"`
+		Enabled   bool      `json:"enabled,omitempty"`
+		CreatedAt time.Time `json:"created_at,omitempty"`
+		UpdatedAt time.Time `json:"updated_at,omitempty"`
+	}
+
+	// Commonly used structure for users
+	UserCommon struct {
+		Username    string    `json:"user_name,omitempty"`
+		Password    string    `json:"password,omitempty"`
+		DisplayName string    `json:"display_name,omitempty"`
+		FirstName   string    `json:"first_name,omitempty"`
+		LastName    string    `json:"last_name,omitempty"`
+		Email       string    `json:"email,omitempty"`
+		URL         string    `json:"url,omitempty"`
+		Activated   string    `json:"activated,omitempty"`
+		LastLogin   time.Time `json:"last_login,omitempty"`
+	}
+
+	// Image structure
+	Image struct {
+		URL    string `json:"url,omitempty"`
+		Type   string `json:"type,omitempty"`
+		Width  string `json:"width,omitempty"`
+		Heigth string `json:"height,omitempty"`
+	}
+
+	// Object structure
+	Object struct {
+		ID          string    `json:"id,omitempty"`
+		Type        string    `json:"type,omitempty"`
+		URL         string    `json:"url,omitempty"`
+		DisplayName []*string `json:"display_name,omitempty"`
+	}
+
+	// Object structure
+	Participant struct {
+		ID          string    `json:"id,omitempty"`
+		URL         string    `json:"url,omitempty"`
+		DisplayName []*string `json:"display_name,omitempty"`
+		Image       []*Image
+	}
 
 	// Account structure
 	Account struct {
-		ID         uint64         `json:"id"`
-		Name       string         `json:"name"`
-		AcountUser []*AccountUser `json:"account_user,omitempty" db:"-"`
-		Enabled    bool           `json:"enabled"`
-		CreatedAt  time.Time      `json:"created_at" db:"created_at"`
-		UpdatedAt  time.Time      `json:"updated_at" db:"updated_at"`
+		ID          uint64 `json:"id,omitempty"`
+		Name        string `json:"name,omitempty"`
+		Description string `json:"description,omitempty"`
+		Enabled     bool   `json:"enabled,omitempty"`
+		Common
+	}
+
+	// AccountRole structure
+	AccountRole struct {
+		ID          uint64 `json:"id,omitempty"`
+		Permission  string `json:"permission,omitempty"`
+		Description string `json:"description,omitempty"`
+		Common
 	}
 
 	// AccountUser structure
 	AccountUser struct {
-		ID        uint64    `json:"id"`
-		AccountID uint64    `json:"account_id" db:"account_id"`
-		Name      string    `json:"name"`
-		Password  string    `json:"password"`
-		Email     string    `json:"email"`
-		Enabled   bool      `json:"enabled"`
-		LastLogin time.Time `json:"last_login,omitempty" db:"last_login"`
-		CreatedAt time.Time `json:"created_at" db:"created_at"`
-		UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+		ID        uint64 `json:"id,omitempty"`
+		AccountID uint64 `json:"account_id,omitempty"`
+		Role      *AccountRole
+		UserCommon
+		Common
 	}
 
 	// Application structure
 	Application struct {
-		ID        uint64    `json:"id"`
-		Key       string    `json:"key"`
-		Name      string    `json:"name"`
-		AccountID uint64    `json:"account_id" db:"account_id"`
-		Enabled   bool      `json:"enabled"`
-		CreatedAt time.Time `json:"created_at" db:"created_at"`
-		UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+		ID          uint64 `json:"id,omitempty"`
+		AccountID   uint64 `json:"account_id,omitempty"`
+		AuthToken   string `json:"auth_token,omitempty"`
+		Name        string `json:"name,omitempty"`
+		Description string `json:"description,omitempty"`
+		URL         string `json:"url,omitempty"`
+		Common
 	}
 
 	// User structure
 	User struct {
-		AppID        uint64    `json:"app_id,omitempty" db:"application_id"`
-		Token        string    `json:"token,omitempty"`
-		Username     string    `json:"username,omitempty"`
-		Name         string    `json:"name",omitempty"`
-		Password     string    `json:"password,omitempty"`
-		Email        string    `json:"email,omitempty"`
-		URL          string    `json:"url,omitempty" db:"url"`
-		ThumbnailURL string    `json:"thumbnail_url,omitempty" db:"thumbnail_url"`
-		Provider     string    `json:"provider,omitempty"`
-		Custom       string    `json:"custom,omitempty"`
-		Connections  []*User   `json:"connections,omitempty" db:"-"`
-		Events       []*Event  `json:"events,omitempty" db:"-"`
-		LastLogin    time.Time `json:"last_login,omitempty" db:"last_login"`
-		CreatedAt    time.Time `json:"created_at" db:"created_at"`
-		UpdatedAt    time.Time `json:"updated_at" db:"updated_at"`
+		ID            uint64    `json:"id",omitempty`
+		ApplicationID uint64    `json:"application_id,omitempty"`
+		AuthToken     string    `json:"auth_token,omitempty"`
+		FacebookID    string    `json:"facebook_id,omitempty"`
+		TwitterID     string    `json:"twitter_id,omitempty"`
+		GoogleID      string    `json:"google_id,omitempty"`
+		CustomerID    string    `json:"customer_id,omitempty"`
+		GameCenterID  string    `json:"game_center_id,omitempty"`
+		DeviceIDs     []*string `json:"device_ids,omitempty"`
+		URL           string    `json:"url,omitempty"`
+		UserCommon
+		Common
 	}
 
-	// UserConnection structure holds the connections of the users between each-other
-	UserConnection struct {
-		AppID      string    `json:"app_id" db:"application_id"`
-		UserToken1 string    `json:"user_id1" db:"user_id1"`
-		UserToken2 string    `json:"user_id2" db:"user_id2"`
-		Enabled    bool      `json:"enabled"`
-		CreatedAt  time.Time `json:"created_at" db:"created_at"`
-		UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
+	// Connection structure holds the connections of the users
+	Connection struct {
+		ApplicationID string `json:"application_id,omitempty"`
+		UserFromID    string `json:"user_from_id,omitempty"`
+		UserToID      string `json:"user_to_id,omitempty"`
+		Common
 	}
 
 	// Device structure
 	Device struct {
-		GID          string `json:"gid" db:"gid"`
-		Model        string `json:"model"`
-		Manufacturer string `json:"manufacturer"`
-		UUID         string `json:"uuid" db:"uuid"`
-		IDFA         string `json:"idfa" db:"idfa"`
-		IDFV         string `json:"idfv" db:"idfv"`
-		Mac          string `json:"mac" db:"mac"`
-		MacMD5       string `json:"mac_md5" db:"mac_md5"`
-		MacSHA1      string `json:"mac_sha1" db:"mac_sha1"`
-		AndroidID    string `json:"android_id" db:"android_id"`
-		GPSAdID      string `json:"gps_adid" db:"gps_adid"`
-		Platform     string `json:"platfrom"`
-		OSVersion    string `json:"os_version" db:"os_version"`
-		Browser      string `json:"browser" db:"browser"`
-		AppVersion   string `json:"app_version" db:"app_version"`
-		SDKVersion   string `json:"sdk_version" db:"sdk_version"`
-		Timezone     string `json:"timezone"`
-		Language     string `json:"language"`
-		Country      string `json:"country"`
-		City         string `json:"city"`
-		IP           string `json:"ip" db:"ip"`
-		Carrier      string `json:"carrier"`
-		Network      string `json:"network"`
-	}
-
-	// Session structure
-	Session struct {
-		ID        uint64 `json:"id"`
-		AppID     uint64 `json:"app_id" db:"application_id"`
-		UserToken string `json:"user_token" db:"user_token"`
-		Nth       uint64 `json:"nth"`
-		Custom    string `json:"custom,omitempty"`
-		Device
-		User      *User     `json:"user,omitempty" db:"-"`
-		Events    []*Event  `json:"events,omitempty" db:"-"`
-		CreatedAt time.Time `json:"created_at" db:"created_at"`
-		UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
-	}
-
-	// Item structure
-	Item struct {
-		ID           string `json:"item_id" db:"item_id"`
-		Name         string `json:"item_name,omitempty" db:"item_name"`
-		URL          string `json:"item_url,omitempty" db:"item_url"`
-		ThumbnailURL string `json:"thumbnail_url,omitempty" db:"thumbnail_url"`
+		ID           string `json:"id,omitempty"`
+		UUID         string `json:"uuid,omitempty"`
+		IDFA         string `json:"idfa,omitempty"`
+		IDFV         string `json:"idfv,omitempty"`
+		GPSAdID      string `json:"gps_adid,omitempty"`
+		AndroidID    string `json:"android_id,omitempty"`
+		PushToken    string `json:"push_token,omitempty"`
+		Mac          string `json:"mac,omitempty"`
+		MacMD5       string `json:"mac_md5,omitempty"`
+		MacSHA1      string `json:"mac_sha1,omitempty"`
+		Platform     string `json:"platfrom,omitempty"`
+		OSVersion    string `json:"os_version,omitempty"`
+		Browser      string `json:"browser,omitempty"`
+		Model        string `json:"model,omitempty"`
+		Manufacturer string `json:"manufacturer,omitempty"`
+		AppVersion   string `json:"app_version,omitempty"`
+		SDKVersion   string `json:"sdk_version,omitempty"`
+		Timezone     string `json:"timezone,omitempty"`
+		Language     string `json:"language,omitempty"`
+		Country      string `json:"country,omitempty"`
+		City         string `json:"city,omitempty"`
+		IP           string `json:"ip,omitempty"`
+		Carrier      string `json:"carrier,omitempty"`
+		Network      string `json:"network,omitempty"`
+		Common
 	}
 
 	// Event structure
 	Event struct {
-		ID        uint64 `json:"id"`
-		AppID     uint64 `json:"app_id,omitempty" db:"application_id"`
-		SessionID uint64 `json:"session_id" db:"session_id"`
-		UserToken string `json:"user_token,omitempty" db:"user_token"`
-		Title     string `json:"title",omitempty" db:"title"`
-		Type      string `json:"type"`
-		Item
-		Custom    string    `json:"custom,omitempty"`
-		Nth       uint64    `json:"nth"`
-		User      *User     `json:"user,omitempty" db:"-"`
-		CreatedAt time.Time `json:"created_at" db:"created_at"`
+		ID            uint64 `json:"id,omitempty"`
+		ApplicationID uint64 `json:"application_id,omitempty"`
+		UserID        uint64 `json:"user_id,omitempty"`
+		Verb          string `json:"string,omitempty"`
+		Language      string `json:"language,omitempty"`
+		Prioritity    string `json:"priority,omitempty"`
+		Status        string `json:"status,omitempty"`
+		Location      string `json:"location,omitempty"`
+		Object        *Object
+		Target        *Object
+		Instrument    *Object
+		Attachment    *Object
+		Participant   *Participant
+		Common
 	}
 )
