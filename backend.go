aedabcaf19b8f5bea6801a9cf6584fc55a144410
Onur Akpolat <onur.akpolat@gmail.com>
2015-02-02 01:16:19 +0100

    cleanup

--- backend.go
+++ backend.go
@@ -61,6 +61,21 @@ func main() {
 	// Get router
 	router := server.GetRouter(conf.Environment != "prod", newRelicAgent)
 
+	log.Printf(`
+
+88888888888                         888                          .d8888b.                                             
+    888                             888                         d88P  Y88b                                            
+    888                             888                         Y88b.                                                 
+    888   8888b.  88888b.   .d88b.  888 888  888  .d88b.         "Y888b.    .d88b.  888d888 888  888  .d88b.  888d888 
+    888      "88b 888 "88b d88P"88b 888 888  888 d8P  Y8b           "Y88b. d8P  Y8b 888P"   888  888 d8P  Y8b 888P"   
+    888  .d888888 888  888 888  888 888 888  888 88888888             "888 88888888 888     Y88  88P 88888888 888     
+    888  888  888 888 d88P Y88b 888 888 Y88b 888 Y8b.           Y88b  d88P Y8b.     888      Y8bd8P  Y8b.     888     
+    888  "Y888888 88888P"   "Y88888 888  "Y88888  "Y8888         "Y8888P"   "Y8888  888       Y88P    "Y8888  888     
+                  888           888                                                                                   
+                  888      Y8b d88P                                                                                   
+                  888       "Y88P"
+     
+  	`)
 	log.Printf("Starting the server at port %s", conf.ListenHostPort)
 	log.Fatal(http.ListenAndServe(conf.ListenHostPort, router))
 }
