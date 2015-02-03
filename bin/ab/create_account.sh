7c89883a4ab436380a9bd92889d4ff4b466f3b06
Florin Patan <florinpatan@gmail.com>
2015-02-03 18:00:24 +0100

    Improve ab script

--- bin/ab/create_account.sh
+++ bin/ab/create_account.sh
@@ -6,12 +6,14 @@ echo "Building the backend app"
 go build -o backend backend.go
 
 echo "Launching the backend app"
-./backend &
+2>/dev/null 1>&2 ./backend &
 sleep 1
 
 echo "Starting the siege"
-ab -n 200000 -c 100 -p resources/ab_test/new_account_payload  -T 'application/json' localhost:8082/0.1/accounts &
+ab -n 500000 -c 100 -p resources/ab_test/new_account_payload  -T 'application/json' localhost:8082/0.1/accounts &
 sleep 3
 
 echo "Starting pprof"
 go tool pprof backend http://localhost:8082/debug/pprof/profile
+
+kill `pidof backend`
