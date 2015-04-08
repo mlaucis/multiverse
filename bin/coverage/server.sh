b84215a6f89381269a0c8126f71723f056655c2d
Florin Patan <florinpatan@gmail.com>
2015-04-09 00:35:01 +0200

    Misc changes

--- bin/coverage/server.sh
+++ bin/coverage/server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ${GOPATH}/src/github.com/tapglue/backend/v01/server/
-gocov test -race -coverpkg=github.com/tapglue/backend/v01/server,github.com/tapglue/backend/v01/core > coverage.json
+gocov test -race -coverpkg=github.com/tapglue/backend/v01/server,github.com/tapglue/backend/v01/core,github.com/tapglue/backend/v01/context,github.com/tapglue/backend/v01/storage,github.com/tapglue/backend/v01/storage/redis,github.com/tapglue/backend/v01/validator,github.com/tapglue/backend/v01/validator/keys,github.com/tapglue/backend/v01/validator/tokens,github.com/tapglue/backend/v01/fixtures,github.com/tapglue/backend/v01/entity > coverage.json
 gocov-html coverage.json > coverage.html
 
 case "$(uname -s)" in
@@ -18,7 +18,7 @@ sleep 3
 rm coverage.json coverage.html
 
 cd ${GOPATH}/src/github.com/tapglue/backend/v02/server/
-gocov test -race > coverage.json
+gocov test -race -coverpkg=github.com/tapglue/backend/v02/server,github.com/tapglue/backend/v02/core,github.com/tapglue/backend/v02/context,github.com/tapglue/backend/v02/storage,github.com/tapglue/backend/v02/storage/redis,github.com/tapglue/backend/v02/validator,github.com/tapglue/backend/v02/validator/keys,github.com/tapglue/backend/v02/validator/tokens,github.com/tapglue/backend/v02/fixtures,github.com/tapglue/backend/v02/entity > coverage.json
 gocov-html coverage.json > coverage.html
 
 case "$(uname -s)" in
