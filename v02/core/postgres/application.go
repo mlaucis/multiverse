2bbab946c9752064639e153dcc8a36ccc3f96e65
Florin Patan <florinpatan@gmail.com>
2015-04-28 10:52:07 +0200

    Support delete operations for apps

--- v02/core/postgres/application.go
+++ v02/core/postgres/application.go
@@ -23,11 +23,34 @@ type (
 )
 
 const (
-	createApplicationEntryQuery           = `INSERT INTO applications (account_id, json_data) VALUES($1, $2) RETURNING id`
-	selectApplicationEntryByIDQuery       = `SELECT json_data FROM applications WHERE id = $1 AND account_id = $2`
+	createApplicationEntryQuery     = `INSERT INTO applications (account_id, json_data) VALUES($1, $2) RETURNING id`
+	createApplicationNamespaceQuery = `CREATE DATABASE app_$1_$2;
+	CREATE TABLE app_$1_$2.users
+	(
+		id SERIAL PRIMARY KEY NOT NULL,
+		json_data JSONB NOT NULL,
+		enabled INT DEFAULT 1 NOT NULL
+	);
+	CREATE TABLE app_$1_$2.events
+	(
+		id SERIAL PRIMARY KEY NOT NULL,
+		json_data JSONB NOT NULL,
+		enabled INT DEFAULT 1 NOT NULL
+	);
+	CREATE TABLE app_$1_$2.connections
+	(
+		json_data JSONB NOT NULL,
+		enabled INT DEFAULT 1 NOT NULL
+	);
+
+	CREATE INDEX on app_$1_$2.users USING GIN (json_data jsonb_path_ops);
+	CREATE INDEX on app_$1_$2.events USING GIN (json_data jsonb_path_ops);
+	CREATE INDEX on app_$1_$2.connections USING GIN (json_data jsonb_path_ops);
+	`
+	selectApplicationEntryByIDQuery       = `SELECT json_data, enabled FROM applications WHERE id = $1 AND account_id = $2`
 	updateApplicationEntryByIDQuery       = `UPDATE applications SET json_data = $1 WHERE id = $2 AND account_id = $3`
-	deleteApplicationEntryByIDQuery       = `DELETE FROM applications WHERE id = $1`
-	listApplicationsEntryByAccountIDQuery = `SELECT id, json_data FROM applications where account_id = $1`
+	deleteApplicationEntryByIDQuery       = `UPDATE applications SET enabled = 1 WHERE id = $1 AND account_id = $2`
+	listApplicationsEntryByAccountIDQuery = `SELECT id, json_data, enabled FROM applications where account_id = $1`
 )
 
 func (app *application) Create(application *entity.Application, retrieve bool) (*entity.Application, errors.Error) {
@@ -50,8 +73,8 @@ func (app *application) Create(application *entity.Application, retrieve bool) (
 
 func (app *application) Read(accountID, applicationID int64) (*entity.Application, errors.Error) {
 	applicationJSON := &struct {
-		ID       int64
 		JSONData string
+		Enabled  bool
 	}{}
 	err := app.pg.SlaveDatastore(-1).
 		QueryRow(selectApplicationEntryByIDQuery, applicationID, accountID).
@@ -65,7 +88,8 @@ func (app *application) Read(accountID, applicationID int64) (*entity.Applicatio
 	if err != nil {
 		return nil, errors.NewInternalError("error while reading application", err.Error())
 	}
-	application.ID = applicationJSON.ID
+	application.ID = applicationID
+	application.Enabled = applicationJSON.Enabled
 
 	return application, nil
 }
@@ -92,9 +116,9 @@ func (app *application) Update(existingApplication, updatedApplication entity.Ap
 }
 
 func (app *application) Delete(application *entity.Application) errors.Error {
-	_, err := app.mainPg.Exec(deleteApplicationEntryByIDQuery, application.ID)
+	_, err := app.mainPg.Exec(deleteApplicationEntryByIDQuery, application.ID, application.AccountID)
 	if err != nil {
-		return errors.NewInternalError("error while deleting the account", err.Error())
+		return errors.NewInternalError("error while deleting the application", err.Error())
 	}
 	return nil
 }
@@ -111,6 +135,7 @@ func (app *application) List(accountID int64) ([]*entity.Application, errors.Err
 		applicationJSON := &struct {
 			ID       int64
 			JSONData string
+			Enabled  bool
 		}{}
 		err := rows.Scan(applicationJSON)
 		if err != nil {
@@ -122,6 +147,7 @@ func (app *application) List(accountID int64) ([]*entity.Application, errors.Err
 			return []*entity.Application{}, errors.NewInternalError("failed to read the applications", err.Error())
 		}
 		application.ID = applicationJSON.ID
+		application.Enabled = applicationJSON.Enabled
 
 		applications = append(applications, application)
 	}
