f6c1cb9eef3420d97e00e851daa5a1dbaa2023ca
Florin Patan <florinpatan@gmail.com>
2015-05-26 18:20:57 +0200

    Update SQL schema

--- resources/pgsql.sql
+++ resources/pgsql.sql
@@ -1,3 +1,30 @@
+CREATE SCHEMA IF NOT EXISTS public;
+
+CREATE EXTENSION postgis;
+CREATE EXTENSION postgis_topology;
+CREATE EXTENSION fuzzystrmatch;
+CREATE EXTENSION postgis_tiger_geocoder;
+
+ALTER SCHEMA topology
+OWNER TO rds_superuser;
+ALTER SCHEMA tiger
+OWNER TO rds_superuser;
+
+CREATE FUNCTION exec(TEXT)
+  RETURNS TEXT LANGUAGE plpgsql VOLATILE AS $f$ BEGIN EXECUTE $1;
+  RETURN $1;
+END; $f$;
+SELECT exec('ALTER TABLE ' || quote_ident(s.nspname) || '.' || quote_ident(s.relname) || ' OWNER TO rds_superuser')
+FROM (
+  SELECT
+    nspname,
+    relname
+  FROM pg_class c JOIN pg_namespace n ON (c.relnamespace = n.oid)
+  WHERE nspname IN ('tiger', 'topology') AND
+        relkind IN ('r', 'S', 'v')
+  ORDER BY relkind = 'S');
+
+
 CREATE SCHEMA tg;
 
 CREATE TABLE tg.accounts (
@@ -25,7 +52,7 @@ CREATE TABLE tg.applications (
   enabled INT DEFAULT 1 NOT NULL
 );
 
-CREATE INDEX on tg.accounts USING GIN (json_data jsonb_path_ops);
-CREATE INDEX on tg.account_users USING GIN (json_data jsonb_path_ops);
-CREATE INDEX on tg.applications USING GIN (json_data jsonb_path_ops);
+CREATE INDEX on tg.accounts USING GIN (json_data);
+CREATE INDEX on tg.account_users USING GIN (json_data);
+CREATE INDEX on tg.applications USING GIN (json_data);
 
