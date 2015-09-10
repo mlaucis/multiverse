c05b1c16a4e6975166f183834829929456cb5c7a
Florin Patan <florinpatan@gmail.com>
2015-09-10 14:40:25 +0200

    Add Dawanda subdomain

--- terraform/prod/customers.tf
+++ terraform/prod/customers.tf
@@ -6,3 +6,12 @@ resource "cloudflare_record" "dailyme" {
   type = "CNAME"
   ttl = 1
 }
+
+resource "cloudflare_record" "dawanda" {
+  domain = "${var.cloudflare_domain}"
+  name = "dawanda"
+  #  value = "${aws_elb.frontend.dns_name}"
+  value = "staging-347078730.eu-central-1.elb.amazonaws.com"
+  type = "CNAME"
+  ttl = 1
+}
