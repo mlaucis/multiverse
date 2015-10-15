279ba9610d0de4e55d764f0e0e6cfb2595021d1b
Alexander Simmerl <alx@soundcloud.com>
2015-10-15 16:30:25 +0200

    Correct relative paths for certs

--- infrastructure/terraform/prod/certificates.tf
+++ infrastructure/terraform/prod/certificates.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_server_certificate" "self-signed" {
   name = "self-signed"
-  certificate_body = "${file("../certs/self/self.crt")}"
-  private_key = "${file("../certs/self/self.key")}"
+  certificate_body = "${file("../../certs/self/self.crt")}"
+  private_key = "${file("../../certs/self/self.key")}"
 }
