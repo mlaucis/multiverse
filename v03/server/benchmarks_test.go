6f9335925344eb706d43562fa20172d6ec1e8b24
Florin Patan <florinpatan@gmail.com>
2015-10-15 11:27:32 +0200

    Add function to create events for existing users

--- v03/server/benchmarks_test.go
+++ v03/server/benchmarks_test.go
@@ -6,7 +6,6 @@ package server_test
 import (
 	"flag"
 	"log"
-
 	"os"
 
 	. "gopkg.in/check.v1"
@@ -14,14 +13,16 @@ import (
 
 var (
 	organizations    = flag.Int("org", 50, "number of organizations")
+	organizationID   = flag.Int64("orgid", 1, "organization id")
 	members          = flag.Int("orgmem", 5, "number of members per organization")
 	applications     = flag.Int("app", 1, "number of applications per organization")
+	applicationID    = flag.Int64("appid", 1, "application id")
 	applicationUsers = flag.Int("appusr", 100000, "number of application users")
 	connections      = flag.Int("conn", 10, "number of connections per application user (max 50 or appusr)")
 	events           = flag.Int("evt", 500, "number of events per application user")
 )
 
-func (s *EventSuite) TestCreateUsers(c *C) {
+func (s *BenchSuite) TestCreateUsers(c *C) {
 	flag.Parse()
 
 	if os.Getenv("CI") == "true" {
@@ -40,4 +41,17 @@ func (s *EventSuite) TestCreateUsers(c *C) {
 		*organizations, *members, *applications, *applicationUsers, *connections, *events)
 
 	CorrectDeployBench(*organizations, *members, *applications, *applicationUsers, *connections, *events, true, true)
+}
+
+func (s *BenchSuite) TestCreateUserEvents(c *C) {
+	flag.Parse()
+
+	if os.Getenv("CI") == "true" {
+		c.Skip("not to be run inside the CI suite")
+	}
+
+	log.Printf("\n\nCreating events for organization = %d, application = %d\n\n",
+		*organizationID, *applicationID)
+
+	populateEventsForUsers(*organizationID, *applicationID, *events)
 }
