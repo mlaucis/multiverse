340eca1da749caed4824436014787bb69b2a3745
Alexander Simmerl <alx@soundcloud.com>
2015-10-16 12:26:21 +0200

    Use relative path for certs

--- infrastructure/terraform/staging/certificates.tf
+++ infrastructure/terraform/staging/certificates.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_server_certificate" "self-signed-staging" {
   name = "self-signed-staging"
-  certificate_body = "${file("../../certs/self/self.crt")}"
-  private_key = "${file("../../certs/self/self.key")}"
+  certificate_body = "${file("${path.module}/../../certs/self/self.crt")}"
+  private_key = "${file("${path.module}/../../certs/self/self.key")}"
 }
