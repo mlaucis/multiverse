aad1eb0319ede9378689fd698498efe18e71bab4
Alexander Simmerl <alx@soundcloud.com>
2015-11-09 17:13:20 +0100

    Add intaker rules for quick dashboard

--- infrastructure/build/scripts/install_prometheus.sh
+++ infrastructure/build/scripts/install_prometheus.sh
@@ -16,9 +16,14 @@ sudo apt-get install -y prometheus node-exporter pushgateway alertmanager
 sudo service prometheus start
 sudo update-rc.d prometheus defaults 95 10
 
+# /etc/prometheus/prometheus.yml
 echo '
 global:
-  scrape_interval: "15s"
+  evaluation_interval: "1m"
+  scrape_interval: "30s"
+
+rule_files:
+  - /etc/prometheus/api.rules
 
 scrape_configs:
   - job_name: "prometheus"
@@ -34,10 +39,6 @@ scrape_configs:
     target_groups:
     - targets:
         - "localhost:9093"
-  - job_name: "node"
-    target_groups:
-    - targets:
-        - "localhost:9100"
 
   - job_name: "node-exporter"
     ec2_sd_configs:
@@ -53,3 +54,14 @@ scrape_configs:
         secret_key: Z/YsT+kX4wgfytuvfWBOlzwHGGmivjwtZn2W6oHs
         port: 9000
 ' | sudo tee /etc/prometheus/prometheus.yml > /dev/null
+
+# /etc/prometheus/api.rules
+ehco '
+job:intaker_request_statuss:sum = sum by (status) (rate(api_intaker_request_count [1m]))
+job:intaker_request_latency:avg = avg(api_intaker_request_latency_microseconds_sum / api_intaker_request_latency_microseconds_count)
+job:intaker_request_latency:max = max(api_intaker_request_latency_microseconds_sum / api_intaker_request_latency_microseconds_count)
+job:intaker_request_latency:min = min(api_intaker_request_latency_microseconds_sum / api_intaker_request_latency_microseconds_count)
+job:intaker_request_routes:topk = topk(5, sum(rate(api_intaker_request_count [5m])) by (route))
+job:platform_process_res:sum = sum(process_resident_memory_bytes) by (instance, job)
+job:platform_process_cpu:max = max(rate(process_cpu_seconds_total [5m])) by (instance, job)
+' | sudo tee /etc/prometheus/api.rules > /dev/null
