24e26adaecff3e05abe9849a0a8343747a7b55ce
Alexander Simmerl <alx@soundcloud.com>
2015-11-27 17:08:16 +0100

    Use json un/marshaler interface for intermediate type
    Instead of having to rely of the use of helper functions we equip the
    intermediate type used to accept and return Object representations in
    HTTP with the expected interface implementations in order for the json
    package to work.
    
    This pattern should be preferred when dealing with encoding/decoding in
    handler code.

--- gateway/http/handler.go
+++ gateway/http/handler.go
@@ -20,30 +20,24 @@ type Handler func(context.Context, http.ResponseWriter, *http.Request)
 func ObjectCreate(c *controller.ObjectController) Handler {
 	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		var (
-			app     = appFromContext(ctx)
-			user    = userFromContext(ctx)
-			rObject = &responseObject{}
+			app  = appFromContext(ctx)
+			user = userFromContext(ctx)
+			ro   = &responseObject{}
 		)
 
-		err := json.NewDecoder(r.Body).Decode(rObject)
+		err := json.NewDecoder(r.Body).Decode(ro)
 		if err != nil {
 			respondError(w, http.StatusInternalServerError, 0, err)
 			return
 		}
 
-		object, err := toObject(rObject)
-		if err != nil {
-			respondError(w, http.StatusBadRequest, 0, err)
-			return
-		}
-
-		object, err = c.Create(app, object, user)
+		object, err := c.Create(app, ro.Object, user)
 		if err != nil {
 			respondError(w, http.StatusInternalServerError, 0, err)
 			return
 		}
 
-		respondJSON(w, http.StatusCreated, fromObject(object))
+		respondJSON(w, http.StatusCreated, &responseObject{Object: object})
 	}
 }
 
@@ -154,7 +148,7 @@ func ObjectRetrieve(c *controller.ObjectController) Handler {
 			return
 		}
 
-		respondJSON(w, http.StatusOK, fromObject(object))
+		respondJSON(w, http.StatusOK, &responseObject{Object: object})
 	}
 }
 
@@ -162,8 +156,8 @@ func ObjectRetrieve(c *controller.ObjectController) Handler {
 func ObjectUpdate(c *controller.ObjectController) Handler {
 	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
 		var (
-			app     = appFromContext(ctx)
-			rObject = &responseObject{}
+			app = appFromContext(ctx)
+			ro  = &responseObject{}
 		)
 
 		id, err := strconv.ParseUint(mux.Vars(r)["objectID"], 10, 64)
@@ -172,19 +166,13 @@ func ObjectUpdate(c *controller.ObjectController) Handler {
 			return
 		}
 
-		err = json.NewDecoder(r.Body).Decode(rObject)
+		err = json.NewDecoder(r.Body).Decode(ro)
 		if err != nil {
 			respondError(w, http.StatusInternalServerError, 0, err)
 			return
 		}
 
-		object, err := toObject(rObject)
-		if err != nil {
-			respondError(w, http.StatusBadRequest, 0, err)
-			return
-		}
-
-		updated, err := c.Update(app, id, object)
+		updated, err := c.Update(app, id, ro.Object)
 		if err != nil {
 			if err == controller.ErrNotFound {
 				respondError(w, http.StatusNotFound, 0, err)
@@ -195,13 +183,17 @@ func ObjectUpdate(c *controller.ObjectController) Handler {
 			return
 		}
 
-		respondJSON(w, http.StatusOK, fromObject(updated))
+		respondJSON(w, http.StatusOK, &responseObject{Object: updated})
 	}
 }
 
 type responseObject struct {
+	*object.Object
+}
+
+type responseObjectFields struct {
 	Attachments []object.Attachment `json:"attachments"`
-	CreatedAt   time.Time           `json:"created_at"`
+	CreatedAt   time.Time           `json:"created_at,omitempty"`
 	ID          string              `json:"id"`
 	Latitude    float64             `json:"latitude"`
 	Location    string              `json:"location"`
@@ -210,64 +202,74 @@ type responseObject struct {
 	Tags        []string            `json:"tags"`
 	TargetID    string              `json:"target_id,omitempty"`
 	Type        string              `json:"type"`
-	UpdatedAt   time.Time           `json:"updated_at"`
+	UpdatedAt   time.Time           `json:"updated_at,omitempty"`
 	UserID      string              `json:"user_id"`
 	Visibility  object.Visibility   `json:"visibility"`
 }
 
-func fromObject(o *object.Object) *responseObject {
-	r := &responseObject{
-		Attachments: o.Attachments,
-		CreatedAt:   o.CreatedAt,
-		ID:          strconv.FormatUint(o.ID, 10),
-		Latitude:    o.Latitude,
-		Location:    o.Location,
-		Longitude:   o.Longitude,
-		Tags:        o.Tags,
-		TargetID:    o.TargetID,
-		Type:        o.Type,
-		UpdatedAt:   o.UpdatedAt,
-		UserID:      strconv.FormatUint(o.OwnerID, 10),
-		Visibility:  o.Visibility,
-	}
+func (ro *responseObject) MarshalJSON() ([]byte, error) {
+	var (
+		o = ro.Object
+		r = responseObjectFields{
+			Attachments: o.Attachments,
+			CreatedAt:   o.CreatedAt,
+			ID:          strconv.FormatUint(o.ID, 10),
+			Latitude:    o.Latitude,
+			Location:    o.Location,
+			Longitude:   o.Longitude,
+			Tags:        o.Tags,
+			TargetID:    o.TargetID,
+			Type:        o.Type,
+			UpdatedAt:   o.UpdatedAt,
+			UserID:      strconv.FormatUint(o.OwnerID, 10),
+			Visibility:  o.Visibility,
+		}
+	)
 
 	if o.ObjectID > 0 {
 		r.ObjectID = strconv.FormatUint(o.ObjectID, 10)
 	}
 
-	return r
+	return json.Marshal(r)
+}
+
+func (ro *responseObject) UnmarshalJSON(raw []byte) error {
+	f := responseObjectFields{}
+
+	err := json.Unmarshal(raw, &f)
+	if err != nil {
+		return err
+	}
+
+	ro.Object = &object.Object{
+		Attachments: f.Attachments,
+		Latitude:    f.Latitude,
+		Location:    f.Location,
+		Longitude:   f.Longitude,
+		Tags:        f.Tags,
+		TargetID:    f.TargetID,
+		Type:        f.Type,
+		Visibility:  f.Visibility,
+	}
+
+	if f.ObjectID != "" {
+		oID, err := strconv.ParseUint(f.ObjectID, 10, 64)
+		if err != nil {
+			return err
+		}
+
+		ro.Object.ObjectID = oID
+	}
+
+	return nil
 }
 
 func fromObjects(os []*object.Object) []*responseObject {
 	rs := []*responseObject{}
 
 	for _, o := range os {
-		rs = append(rs, fromObject(o))
+		rs = append(rs, &responseObject{Object: o})
 	}
 
 	return rs
-}
-
-func toObject(r *responseObject) (*object.Object, error) {
-	o := &object.Object{
-		Attachments: r.Attachments,
-		Latitude:    r.Latitude,
-		Location:    r.Location,
-		Longitude:   r.Longitude,
-		Tags:        r.Tags,
-		TargetID:    r.TargetID,
-		Type:        r.Type,
-		Visibility:  r.Visibility,
-	}
-
-	if r.ObjectID != "" {
-		oID, err := strconv.ParseUint(r.ObjectID, 10, 64)
-		if err != nil {
-			return nil, err
-		}
-
-		o.ObjectID = oID
-	}
-
-	return o, nil
 }
