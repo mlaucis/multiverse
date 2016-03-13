a1626947b64331af9862cea70fd571fe720e53e4
Alexander Simmerl <alx@soundcloud.com>
2016-03-12 21:15:46 -0500

    Deploy gateway-http#1741

--- infrastructure/terraform/prod/ecs.tf
+++ infrastructure/terraform/prod/ecs.tf
@@ -254,7 +254,7 @@ resource "aws_ecs_task_definition" "dashboard" {
 [
   {
     "name": "dashboard",
-    "image": "775034650473.dkr.ecr.us-east-1.amazonaws.com/dashboard:1739",
+    "image": "775034650473.dkr.ecr.us-east-1.amazonaws.com/dashboard:1741",
     "cpu": 256,
     "memory": 256,
     "essential": true,
@@ -298,7 +298,7 @@ resource "aws_ecs_task_definition" "gateway-http" {
 [
   {
     "name": "gateway-http",
-    "image": "775034650473.dkr.ecr.us-east-1.amazonaws.com/gateway-http:1739",
+    "image": "775034650473.dkr.ecr.us-east-1.amazonaws.com/gateway-http:1741",
     "cpu": 512,
     "memory": 2048,
     "essential": true,
