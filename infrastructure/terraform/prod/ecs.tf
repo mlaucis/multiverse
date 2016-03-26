17804623a6af4ba590fd6bd95c9f98f81a4a7828
Alexander Simmerl <alx@soundcloud.com>
2016-03-26 19:50:21 -0400

    Enable rolling gateway-http deploy

--- infrastructure/terraform/prod/ecs.tf
+++ infrastructure/terraform/prod/ecs.tf
@@ -276,16 +276,16 @@ EOF
 }
 
 resource "aws_ecs_service" "dashboard" {
-  name            = "dashboard"
-  cluster         = "${aws_ecs_cluster.service.id}"
-  task_definition = "${aws_ecs_task_definition.dashboard.arn}"
-  desired_count   = 2
-  iam_role = "${aws_iam_role.ecsELB.arn}"
-  depends_on = [
+  cluster                             = "${aws_ecs_cluster.service.id}"
+  depends_on                          = [
     "aws_iam_role_policy.ecsELB",
   ]
+  desired_count                       = 2
+  iam_role                            = "${aws_iam_role.ecsELB.arn}"
+  name                                = "dashboard"
+  task_definition                     = "${aws_ecs_task_definition.dashboard.arn}"
 
-  load_balancer = {
+  load_balancer {
     elb_name = "${aws_elb.dashboard.id}"
     container_name = "dashboard"
     container_port = 443
@@ -324,16 +324,18 @@ EOF
 }
 
 resource "aws_ecs_service" "gateway-http" {
-  name            = "gateway-http"
-  cluster         = "${aws_ecs_cluster.service.id}"
-  task_definition = "${aws_ecs_task_definition.gateway-http.arn}"
-  desired_count   = 2
-  iam_role = "${aws_iam_role.ecsELB.arn}"
-  depends_on = [
+  cluster                             = "${aws_ecs_cluster.service.id}"
+  depends_on                          = [
     "aws_iam_role_policy.ecsELB",
   ]
+  deployment_maximum_percent          = 200
+  deployment_minimum_healthy_percent  = 50
+  desired_count                       = 2
+  iam_role                            = "${aws_iam_role.ecsELB.arn}"
+  name                                = "gateway-http"
+  task_definition                     = "${aws_ecs_task_definition.gateway-http.arn}"
 
-  load_balancer = {
+  load_balancer {
     elb_name = "${aws_elb.gateway-http.id}"
     container_name = "gateway-http"
     container_port = 8083
