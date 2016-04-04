f1e44817104dd8caf75af780754dfc1f3656dc95
Alexander Simmerl <alx@soundcloud.com>
2016-04-04 16:10:33 -0400

    Make per env monitoring reproducible
    This change introduces reproducible grafana configuration to always
    provide an operational dashboard hooked up to Prometheus and reachable
    via a common public domain (monitoring-$ENV_NAME-$REGION.tapglue.com).
    We also secure access to the dashboards with Google login limited to the
    tapglue.com domain, which is managed via the monitoring GCP project
    which can be managed under the project url[0]. When a new env-region is
    introduced a new callback url has to be introduced[1].
    
    [0] https://console.cloud.google.com/home/dashboard?project=monitoring-1270
    [1] https://console.cloud.google.com/apis/credentials/oauthclient/197936485474-8ahgopooh0f6ui3o082if5ksu2iq45ce.apps.googleusercontent.com?project=monitoring-1270

--- infrastructure/build/scripts/install_prometheus.sh
+++ infrastructure/build/scripts/install_prometheus.sh
@@ -2,8 +2,6 @@
 
 set -ex
 
-env
-
 echo -n 'deb http://deb.robustperception.io/ precise nightly' | sudo tee /etc/apt/sources.list.d/robustperception.io.list > /dev/null
 
 curl https://s3-eu-west-1.amazonaws.com/deb.robustperception.io/41EFC99D.gpg | sudo apt-key add -
@@ -17,43 +15,43 @@ sudo service prometheus start
 sudo update-rc.d prometheus defaults 95 10
 
 # /etc/prometheus/prometheus.yml
-echo '
+echo "
 global:
-  evaluation_interval: "1m"
-  scrape_interval: "30s"
+  evaluation_interval: '1m'
+  scrape_interval: '30s'
 
 rule_files:
   - /etc/prometheus/api.rules
 
 scrape_configs:
-  - job_name: "prometheus"
+  - job_name: 'prometheus'
     target_groups:
     - targets:
-        - "localhost:9090"
-  - job_name: "pushgateway"
+        - 'localhost:9090'
+  - job_name: 'pushgateway'
     honor_labels: true
     target_groups:
     - targets:
-        - "localhost:9091"
-  - job_name: "alertmanager"
+        - 'localhost:9091'
+  - job_name: 'alertmanager'
     target_groups:
     - targets:
-        - "localhost:9093"
+        - 'localhost:9093'
 
-  - job_name: "node-exporter"
+  - job_name: 'node-exporter'
     ec2_sd_configs:
-      - region: "$AWS_REGION"
-        access_key: AKIAJYTAYVGCJR6VNQLA
-        secret_key: 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
+      - region: '$AWS_REGION'
+        access_key: $AWS_ACCESS_KEY
+        secret_key: $AWS_SECRET_KEY
         port: 9100
 
-  - job_name: "services"
+  - job_name: 'gateway-http'
     ec2_sd_configs:
-      - region: "$AWS_REGION"
-        access_key: AKIAJYTAYVGCJR6VNQLA
-        secret_key: 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
+      - region: '$AWS_REGION'
+        access_key: $AWS_ACCESS_KEY
+        secret_key: $AWS_SECRET_KEY
         port: 9000
-' | sudo tee /etc/prometheus/prometheus.yml > /dev/null
+" | sudo tee /etc/prometheus/prometheus.yml > /dev/null
 
 # /etc/prometheus/api.rules
 echo '
@@ -72,6 +70,7 @@ job:gateway_service_op:count = sum(rate(intaker_service_op_count [5m])) by (meth
 job:intaker_request_latency:avg = avg(api_intaker_request_latency_microseconds_sum / api_intaker_request_latency_microseconds_count)
 job:intaker_request_latency:max = max(api_intaker_request_latency_microseconds_sum / api_intaker_request_latency_microseconds_count)
 job:intaker_request_latency:min = min(api_intaker_request_latency_microseconds_sum / api_intaker_request_latency_microseconds_count)
+job:intaker_request_status:sum = sum(rate(api_intaker_request_count [5m])) by (status)
 job:intaker_request_routes:all = sum(rate(api_intaker_request_count [5m])) by (route)
 job:platform_process_res:sum = sum(process_resident_memory_bytes) by (instance, job)
 job:platform_process_cpu:max = max(rate(process_cpu_seconds_total [5m])) by (instance, job)
