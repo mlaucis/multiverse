8865c2c94c0e518d2f7ffedeac00b0d16bd144b1
Alexander Simmerl <alx@soundcloud.com>
2016-05-10 13:47:39 -0400

    Deploy gateway-http#2029

--- infrastructure/terraform/prod/variables.tf
+++ infrastructure/terraform/prod/variables.tf
@@ -9,7 +9,7 @@ variable "region" {
 variable "version" {
   default = {
     dashboard     = "1902"
-    gateway-http  = "2028"
+    gateway-http  = "2029"
     pganalyze     = "1884"
     reporter      = "1945"
   }
