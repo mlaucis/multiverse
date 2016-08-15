5512afa87743d63dee47ab25ab1d46a30851f604
Alexander Simmerl <xla@tapglue.com>
2016-08-15 15:48:05 +0200

    Delete external object handling
    With the feature once requested by a customer long gone we can spare
    ourselves some maintenance overhead by removing all code paths related
    to external object handling which was a convenience for customers to
    create likes and posts on their native inventory.

--- handler/http/comment.go
+++ handler/http/comment.go
@@ -187,139 +187,6 @@ func CommentUpdate(c *controller.CommentController) Handler {
 	}
 }
 
-// ExternalCommentCreate calls the symmetrical controller method.
-func ExternalCommentCreate(c *controller.CommentController) Handler {
-	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		var (
-			externalID = mux.Vars(r)["externalID"]
-			app        = appFromContext(ctx)
-			user       = userFromContext(ctx)
-			p          = &payloadComment{}
-		)
-
-		err := json.NewDecoder(r.Body).Decode(p)
-		if err != nil {
-			respondError(w, 0, wrapError(ErrBadRequest, err.Error()))
-			return
-		}
-
-		comment, err := c.ExternalCreate(app, user.ID, externalID, p.contents)
-		if err != nil {
-			respondError(w, 0, err)
-			return
-		}
-
-		respondJSON(w, http.StatusCreated, &payloadComment{comment: comment})
-	}
-}
-
-// ExternalCommentDelete flags the comment as deleted.
-func ExternalCommentDelete(c *controller.CommentController) Handler {
-	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		var (
-			externalID = mux.Vars(r)["externalID"]
-			app        = appFromContext(ctx)
-			user       = userFromContext(ctx)
-		)
-
-		commentID, err := strconv.ParseUint(mux.Vars(r)["commentID"], 10, 64)
-		if err != nil {
-			respondError(w, 0, wrapError(ErrBadRequest, err.Error()))
-			return
-		}
-
-		err = c.ExternalDelete(app, user.ID, externalID, commentID)
-		if err != nil {
-			respondError(w, 0, err)
-			return
-		}
-
-		respondJSON(w, http.StatusNoContent, nil)
-	}
-}
-
-// ExternalCommentList returns all comments for the given a Post.
-func ExternalCommentList(c *controller.CommentController) Handler {
-	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		var (
-			externalID = mux.Vars(r)["externalID"]
-			app        = appFromContext(ctx)
-		)
-
-		list, err := c.ExternalList(app, externalID)
-		if err != nil {
-			respondError(w, 0, err)
-			return
-		}
-
-		if len(list.Comments) == 0 {
-			respondJSON(w, http.StatusNoContent, nil)
-		}
-
-		respondJSON(w, http.StatusOK, &payloadComments{
-			comments: list.Comments,
-			userMap:  list.UserMap,
-		})
-	}
-}
-
-// ExternalCommentRetrieve returns the comment with the requested id.
-func ExternalCommentRetrieve(c *controller.CommentController) Handler {
-	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		var (
-			externalID = mux.Vars(r)["externalID"]
-			app        = appFromContext(ctx)
-			user       = userFromContext(ctx)
-		)
-
-		commentID, err := strconv.ParseUint(mux.Vars(r)["commentID"], 10, 64)
-		if err != nil {
-			respondError(w, 0, wrapError(ErrBadRequest, err.Error()))
-			return
-		}
-
-		comment, err := c.ExternalRetrieve(app, user.ID, externalID, commentID)
-		if err != nil {
-			respondError(w, 0, err)
-			return
-		}
-
-		respondJSON(w, http.StatusOK, &payloadComment{comment: comment})
-	}
-}
-
-// ExternalCommentUpdate replaces the value for a comment with the new values.
-func ExternalCommentUpdate(c *controller.CommentController) Handler {
-	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-		var (
-			externalID = mux.Vars(r)["externalID"]
-			app        = appFromContext(ctx)
-			user       = userFromContext(ctx)
-			p          = &payloadComment{}
-		)
-
-		commentID, err := strconv.ParseUint(mux.Vars(r)["commentID"], 10, 64)
-		if err != nil {
-			respondError(w, 0, wrapError(ErrBadRequest, err.Error()))
-			return
-		}
-
-		err = json.NewDecoder(r.Body).Decode(p)
-		if err != nil {
-			respondError(w, 0, wrapError(ErrBadRequest, err.Error()))
-			return
-		}
-
-		comment, err := c.ExternalUpdate(app, user.ID, externalID, commentID, p.contents)
-		if err != nil {
-			respondError(w, 0, err)
-			return
-		}
-
-		respondJSON(w, http.StatusOK, &payloadComment{comment: comment})
-	}
-}
-
 type payloadComment struct {
 	contents object.Contents
 	comment  *object.Object
@@ -329,25 +196,23 @@ func (p *payloadComment) MarshalJSON() ([]byte, error) {
 	c := p.comment
 
 	return json.Marshal(struct {
-		Content    string          `json:"content"`
-		Contents   object.Contents `json:"contents"`
-		ExternalID string          `json:"external_id"`
-		ID         string          `json:"id"`
-		PostID     string          `json:"post_id"`
-		Private    *object.Private `json:"private,omitempty"`
-		UserID     string          `json:"user_id"`
-		CreatedAt  time.Time       `json:"created_at"`
-		UpdatedAt  time.Time       `json:"updated_at"`
+		Content   string          `json:"content"`
+		Contents  object.Contents `json:"contents"`
+		ID        string          `json:"id"`
+		PostID    string          `json:"post_id"`
+		Private   *object.Private `json:"private,omitempty"`
+		UserID    string          `json:"user_id"`
+		CreatedAt time.Time       `json:"created_at"`
+		UpdatedAt time.Time       `json:"updated_at"`
 	}{
-		Content:    c.Attachments[0].Contents[object.DefaultLanguage],
-		Contents:   c.Attachments[0].Contents,
-		ExternalID: c.ExternalID,
-		ID:         strconv.FormatUint(c.ID, 10),
-		PostID:     strconv.FormatUint(c.ObjectID, 10),
-		Private:    c.Private,
-		UserID:     strconv.FormatUint(c.OwnerID, 10),
-		CreatedAt:  c.CreatedAt,
-		UpdatedAt:  c.UpdatedAt,
+		Content:   c.Attachments[0].Contents[object.DefaultLanguage],
+		Contents:  c.Attachments[0].Contents,
+		ID:        strconv.FormatUint(c.ID, 10),
+		PostID:    strconv.FormatUint(c.ObjectID, 10),
+		Private:   c.Private,
+		UserID:    strconv.FormatUint(c.OwnerID, 10),
+		CreatedAt: c.CreatedAt,
+		UpdatedAt: c.UpdatedAt,
 	})
 }
 
