da7cd8420ff23e6bab5e4880a6d9afa3d3cd8199
Alexander Simmerl <xla@tapglue.com>
2016-08-30 12:30:00 +0200

    Use latest go version in build script

--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -3,15 +3,15 @@
 set -e
 set -o pipefail
 
-export PATH=/home/ubuntu/.gimme/versions/go1.5.2.linux.amd64/bin:${PATH}
+export PATH=/home/ubuntu/.gimme/versions/go1.7.linux.amd64/bin:${PATH}
 CWD=`pwd`
 
 echo "Installing gimme"
 sudo curl -sL -o /usr/local/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 sudo chmod +x /usr/local/bin/gimme
 
-echo "Installing go 1.5.2"
-eval "$(GIMME_GO_VERSION=1.5.2 gimme)"
+echo "Installing go 1.7"
+eval "$(GIMME_GO_VERSION=1.7 gimme)"
 
 echo "Installing go dependencies"
 go get github.com/tools/godep github.com/axw/gocov/gocov github.com/matm/gocov-html gopkg.in/check.v1
