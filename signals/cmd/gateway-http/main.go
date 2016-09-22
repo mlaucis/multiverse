8ea8ea0254941e7dc0c0820b9ab159511d6fd445
Alexander Simmerl <xla@tapglue.com>
2016-09-22 17:47:12 +0200

    Boostrap app engine service

---
+++ signals/cmd/gateway-http/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/pubsub"
+	"golang.org/x/net/context"
+	"google.golang.org/api/googleapi"
+)
+
+const (
+	envProject = "GCLOUD_PROJECT"
+	envTopic   = "PUBSUB_TOPIC"
+)
+
+func main() {
+	ctx := context.Background()
+
+	client, err := pubsub.NewClient(ctx, mustGetenv(envProject))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	topic, err := client.CreateTopic(ctx, mustGetenv(envTopic))
+	if err != nil {
+		if gErr, ok := err.(*googleapi.Error); !ok || gErr.Code != 409 {
+			log.Fatal(err)
+		}
+	}
+
+	http.HandleFunc("/_ah/health", healthCheckHandler)
+	http.HandleFunc("/track/event", trackEventHandler(topic))
+
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
+
+func trackEventHandler(topic *pubsub.Topic) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ctx = context.Background()
+			msg = &pubsub.Message{
+				Data: []byte("Come on!"),
+			}
+		)
+
+		if _, err := topic.Publish(ctx, msg); err != nil {
+			respondError(w, 500, err)
+			return
+		}
+
+		respondJSON(w, http.StatusNoContent, nil)
+	}
+}
+
+func mustGetenv(key string) string {
+	v := os.Getenv(key)
+
+	if v == "" {
+		log.Fatalf("%s env variable not set", key)
+	}
+
+	return v
+}
+
+type apiError struct {
+	Message string `json:"message"`
+}
+
+func respondError(w http.ResponseWriter, code int, err error) {
+	respondJSON(w, code, apiError{
+		Message: err.Error(),
+	})
+}
+
+func respondJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(payload)
+}
