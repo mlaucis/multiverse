9be44df9d9398f587cce866e4b459fe95f39e9ef
Alexander Simmerl <xla@tapglue.com>
2016-09-23 19:02:41 +0200

    Draft Signal protobufs

--- signals/cmd/gateway-http/main.go
+++ signals/cmd/gateway-http/main.go
@@ -8,8 +8,11 @@ import (
 	"os"
 
 	"cloud.google.com/go/pubsub"
+	"github.com/golang/protobuf/proto"
 	"golang.org/x/net/context"
 	"google.golang.org/api/googleapi"
+
+	"github.com/tapglue/multiverse/signals/pb"
 )
 
 const (
@@ -46,13 +49,17 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 func trackEventHandler(topic *pubsub.Topic) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var (
-			ctx = context.Background()
-			msg = &pubsub.Message{
-				Data: []byte("Come on!"),
-			}
+			ctx    = context.Background()
+			signal = &pb.Signal{}
 		)
 
-		if _, err := topic.Publish(ctx, msg); err != nil {
+		raw, err := proto.Marshal(signal)
+		if err != nil {
+			respondError(w, 500, err)
+			return
+		}
+
+		if _, err := topic.Publish(ctx, &pubsub.Message{Data: raw}); err != nil {
 			respondError(w, 500, err)
 			return
 		}
