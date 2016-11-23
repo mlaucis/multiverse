a051978e10e6139c1f8d5371f9a9095271afe807
Alexander Simmerl <xla@tapglue.com>
2016-11-23 22:57:52 +0100

    Deploy gateway-http#2558

--- infrastructure/terraform/prod/variables.tf
+++ infrastructure/terraform/prod/variables.tf
@@ -9,7 +9,7 @@ variable "region" {
 variable "version" {
   default = {
     dashboard     = "2533"
-    gateway-http  = "2556"
+    gateway-http  = "2558"
     reporter      = "2302"
     sims          = "2549"
   }
